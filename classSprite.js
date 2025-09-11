59d22305e5aa9187ef2fef663133d3878836497d
lyly dev <lyanne.n619@gmail.com>
2025-09-11 11:29:49 +0200

    fix : indentation and spacing for better readability

--- classSprite.js
+++ classSprite.js
@@ -2,44 +2,42 @@
 export default class Sprite {
     elements;
     selector;
-    unit = -230
+    unit = -230;
     currentSprite;
-    divButton=".ButtonsBox"
+    divButton=".ButtonsBox";
 
-    constructor(elements, selector){
-        this.selector= selector;
-        this.elements=elements;
+    constructor(elements, selector) {
+        this.selector = selector;
+        this.elements = elements;
     }
 
-    setAction(state){
-        this.currentSprite= state;
+    setAction(state) {
+        this.currentSprite = state;
         this._changeSprite();
-    };
-
-    clone(selector){
-        return new Sprite(
-            this.elements,selector
-        )
     }
 
-    addButtons(){
+    clone(selector) {
+        return new Sprite(this.elements, selector);
+    }
+
+    addButtons() {
         const spriteBttn = document.querySelector(this.divButton);
-        const addchildDiv = document.createElement("div")
-        const spriteArr = Object.keys(this.elements)
+        const buttonsWrapper = document.createElement("div");
+        const spriteArr = Object.keys(this.elements);
         spriteArr.forEach((property) => {
-            let bttn = document.createElement("button")
-            bttn.textContent = property 
-            bttn.addEventListener("click",()=>{
-                this.setAction(property) 
-            })
-            addchildDiv.appendChild(bttn) 
+            let bttn = document.createElement("button");
+            bttn.textContent = property; 
+            bttn.addEventListener("click", () => {
+                this.setAction(property); 
+            });
+            buttonsWrapper.appendChild(bttn);
         });
-        spriteBttn.appendChild(addchildDiv);
+        spriteBttn.appendChild(buttonsWrapper);
     }
 
     _changeSprite() {
-        let spriteIndex=this.elements.indexOf(this.currentSprite)
-        document.querySelector(this.selector).style.left=`${spriteIndex*this.unit}px`
+        let spriteIndex = this.elements.indexOf(this.currentSprite);
+        document.querySelector(this.selector).style.left = `${spriteIndex * this.unit} px`;
     }
-}
+};
 
--- main.js
+++ main.js
@@ -1,46 +1,46 @@
 import AnimatedSprite from "./classAnimatedSprite";
 import Sprite from "./classSprite";
 
-function awaitDelay(time){
-    return new Promise((resolve) => {
-    setTimeout(() =>  resolve()
-     ,time)})
+function awaitDelay(time) {
+    return new Promise(
+        (resolve) => setTimeout(
+            () =>  resolve(),
+            time,
+        )
+    )
 }
 
 async function scene () {
     faceMotion("");
-    await awaitDelay()
-    faceMotion("")
-}
+    await awaitDelay();
+    faceMotion("");
+};
 
 const testData = [
-    ["sport", "sport",null,2000], 
-    ["playful", "fiesta","living-room",3000],
-    ["rest face", "fiesta",null,5000], 
-    ["butterfly", "fuck",null,0], 
+    ['sport', 'sport', null, 2000], 
+    ['playful', 'fiesta', 'living-room', 3000],
+    ['rest face', 'fiesta', null, 5000], 
+    ['butterfly', 'fuck', null, 0], 
     ];
 
-async function spriteSceneDelay(sceneData,instructionNumber=0,){
-  
-    const sceneInstruction= sceneData[instructionNumber]
-
-    const [faceInstruction,limbsInstruction,backgroundInstruction,timeInstruction] =  sceneInstruction;
+async function spriteSceneDelay(sceneData, instructionNumber = 0) {
+    const sceneInstruction = sceneData[instructionNumber];
+    const [faceInstruction, limbsInstruction, backgroundInstruction, timeInstruction] =  sceneInstruction;
 
     faceMotion(faceInstruction); 
     limbsMotion(limbsInstruction);
-    if(backgroundInstruction) {
-        movetoRoom(backgroundInstruction)
+    if (backgroundInstruction) {
+        movetoRoom(backgroundInstruction);
     } 
-   
-    await awaitDelay(timeInstruction);
-    if(instructionNumber>= sceneData.length-1){
-        return 
+    await awaitDelay(timeInstruction)
+    if (instructionNumber >= sceneData.length - 1) {
+        return; 
     }
-
-    spriteSceneDelay(sceneData,instructionNumber+1); 
+    spriteSceneDelay(sceneData, instructionNumber + 1); 
 };
 
-window.currentRoom = new Sprite( [
+window.currentRoom = new Sprite(
+    [
         "living-room",
         "gym",
         "dressing",
@@ -50,20 +50,22 @@ window.currentRoom = new Sprite( [
         "vending",
         "closed",
         "transition",
-    ],".currentimgBg");
+    ],
+    ".currentimgBg"
+);
 
 window.transitionRoom = window.currentRoom.clone(".transitionimgBg");
 window.nextRoom = window.currentRoom.clone(".newroomimgBg");
 
-function movetoRoom(area){
-    
-    nextRoom.changeRoom(area)
-    
-    const addtransitionClass= document.querySelector(".currentRoom")
-    addtransitionClass.classList.add("movedBackground")
+function movetoRoom(area) {
+    nextRoom.changeRoom(area);
+    const addtransitionClass = document.querySelector(".currentRoom");
+    addtransitionClass.classList.add("movedBackground");
 };
+
 window.limbsSprite = new AnimatedSprite( 
-        {walking: [700, 700],
+    {
+        walking: [700, 700],
         sit: [100],
         neutral: [100],
         typing: [800, 300, 500],
@@ -71,35 +73,43 @@ window.limbsSprite = new AnimatedSprite(
         dance: [600, 900, 600, 1200],
         fuck: [600, 1500,],
         fiesta: [600, 500, 500, 600],
-        kill: [600, 500, 500, 600]}, 
-        ".imgLimbs")
+        kill: [600, 500, 500, 600]
+    }, 
+    ".imgLimbs"
+);
 
-window.faceSprite = new AnimatedSprite({
-    "rest face": [1000, 200],
-    angry: [500, 500],
-    playful : [1000, 200, 400, 400, 400],
-    reading: [1000, 200, 100, 900],
-    sleeping: [500, 500, 500, 500],
-    bewildered: [700, 700, 700],
-    bored: [1000, 700],
-    butterfly:[1200, 300, 300, 300, 300, 4300],
-    shocked: [500, 500],
-    sport:[800, 500, 300, 300, 800],
-    crying: [1200, 300, 300, 300, 300, 300],
-    "mental effort": [ 1200, 300, 300, 300, 300],},
+window.faceSprite = new AnimatedSprite(
+    {
+        "rest face": [1000, 200],
+        angry: [500, 500],
+        playful : [1000, 200, 400, 400, 400],
+        reading: [1000, 200, 100, 900],
+        sleeping: [500, 500, 500, 500],
+        bewildered: [700, 700, 700],
+        bored: [1000, 700],
+        butterfly:[1200, 300, 300, 300, 300, 4300],
+        shocked: [500, 500],
+        sport:[800, 500, 300, 300, 800],
+        crying: [1200, 300, 300, 300, 300, 300],
+        "mental effort": [ 1200, 300, 300, 300, 300]
+    },
     ".imgFace"
-)
+);
 
-window.chestSprite = new Sprite([
-    "basic", 
-    "puffer",
-    "bra",
-    "military",
-    "hoddie",
-    "party",
-    "swimming top",
-    "crop top",
-    "flanel",],".imgChest")
+window.chestSprite = new Sprite(
+    [
+        "basic", 
+        "puffer",
+        "bra",
+        "military",
+        "hoddie",
+        "party",
+        "swimming top",
+        "crop top",
+        "flanel",
+    ],
+    ".imgChest"
+);
 
-window.faceSprite.addButtons()
+window.faceSprite.addButtons();
 
